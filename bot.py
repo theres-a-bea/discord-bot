737b3772728f047526be858612f9533326d62265
theres.a.bea <samuelsson.bea@gmail.com>
2021-12-26 13:54:54 +0800

    pushing

---
+++ bot.py
@@ -0,0 +1,40 @@
+# bot.py
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+import random
+
+#Loads details from .env
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    for guild in bot.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{bot.user} is connected to the following server:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+@bot.command(name='99', help='Responds with a random quote from Brooklyn 99')
+async def nine_nine(ctx):
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    response = random.choice(brooklyn_99_quotes)
+    await ctx.send(response)
+
+bot.run(TOKEN)
